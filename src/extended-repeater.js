27fc177a6a4abda667189bda593cc93acb4b80fd
Pavel Samovich <pavel.samovich@gmail.com>
2020-11-30 02:19:51 +0300

    feat: implement extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,34 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function repeater(str, options) {
+    str = '' + str;
+    let repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|';
+    if (options) {
+        repeatTimes = options.repeatTimes ?? repeatTimes;
+        separator = options.separator ?? separator;
+        addition = options.addition !== undefined ? options.addition : addition;
+        additionRepeatTimes = options.additionRepeatTimes ?? additionRepeatTimes;
+        additionSeparator = options.additionSeparator ?? additionSeparator;
+    }
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+    let finalAddition = '';
+    if (additionRepeatTimes > 0) {
+        finalAddition = addition;
+        while (--additionRepeatTimes > 0) {
+            finalAddition = `${finalAddition}${additionSeparator}${addition}`;
+        }
+    }
+
+    let singleRepetition = finalAddition !== '' ? str + finalAddition : str;
+    let finalString = '';
+    if (repeatTimes > 0) {
+        finalString = singleRepetition;
+        while (--repeatTimes > 0) {
+            finalString = `${finalString}${separator}${singleRepetition}`;
+        }
+    }
+    return finalString;
 };
   
